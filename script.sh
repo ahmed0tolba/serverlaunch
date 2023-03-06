1cde4e87060347ab903e84ac28ccf7741043ee18
ahmed0tolba <79435725+ahmed0tolba@users.noreply.github.com>
2023-03-06 06:36:40 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,28 @@
+# nginx
+sudo apt install nginx -y
+sudo ufw allow 'Nginx Full'
+
+echo 'server {
+    listen 80;
+    server_name iottrafficanalyzer iottrafficanalyzer.com;
+
+    location / {
+        proxy_pass http://127.0.0.1:8000/;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Forwarded-Prefix /;
+    }
+}' 
+>> /etc/nginx/nginx.conf
+
+
+# iot project deploy
+sudo apt-get update
+sudo apt-get install python3-pip -y
+sudo apt-get install tshark -y
+git clone https://github.com/ahmed0tolba/IOTtrafficAnalyzer.git --branch master
+cd IOTtrafficAnalyzer/
+mkdir uploadedFiles
+pip install -r requirements
+
